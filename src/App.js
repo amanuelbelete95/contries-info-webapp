034962d8626338272f4eeb9ef33154f76f6ceede
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-29 10:45:00 +0300

    Add app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Route, Routes } from "react-router-dom";
+import DetailsPage from "./components/DetailsPage";
+import HomePage from "./components/HomePage";
+import "./App.css";
+
+const Layout = () => (
+  <>
+    <Outlet />
+  </>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Layout />}>
+        <Route
+          index
+          element={<HomePage />}
+        />
+        <Route
+          path="/details"
+          element={<DetailsPage />}
+        />
+      </Route>
+    </Routes>
   );
 }
 
